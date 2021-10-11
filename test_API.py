8c1c3abfe20e1f9529513b21bda3847d6c0915b2
Tim Tham <90109466+TWT1989@users.noreply.github.com>
2021-10-11 18:22:11 -0400

    Unit Test for API

---
+++ test_API.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 11:44:08 2021
+
+@author: Tim
+"""
+import unittest
+
+import API
+
+class TestAPI(unittest.TestCase):
+    
+    def test_correct_url(self):
+        
+        
+        self.assertEqual(API.api("TWT1989"), "https://api.github.com/users/TWT1989/repos")
+        self.assertEqual(API.api("richkempinski"), "https://api.github.com/users/richkempinski/repos")
+        
+if __name__ == '__main__':
+    unittest.main()
+    
